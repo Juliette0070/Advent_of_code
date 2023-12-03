720b394d825d1b4f735071e3f2776e48c001b728
Juliette Rousselet <juliette.rousselet@etu.univ-orleans.fr>
2023-12-03 18:37:54 +0100

    Add power function and calculate sum of power values jour 2

--- jour2/code.py
+++ jour2/code.py
@@ -42,5 +42,26 @@ def somme_parties_possibles(nom_fichier):
             somme += num_game(game)
     return somme
 
+def power(liste):
+    max_red = 0
+    max_green = 0
+    max_blue = 0
+    for dico in liste:
+        if dico.get("red",0) > max_red:
+            max_red = dico.get("red",0)
+        if dico.get("green",0) > max_green:
+            max_green = dico.get("green",0)
+        if dico.get("blue",0) > max_blue:
+            max_blue = dico.get("blue",0)
+    return max_red * max_green * max_blue
+
+def somme_power(nom_fichier):
+    dico = make_dico(nom_fichier)
+    somme = 0
+    for _, values in dico.items():
+        somme += power(values)
+    return somme
+
 #condition : 12red, 13green, 14blue 
 print(somme_parties_possibles('jour2/input'))
+print(somme_power('jour2/input'))
