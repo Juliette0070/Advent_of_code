30a9432a66df325db0d90471704a1f20f15776c5
Juliette Rousselet <juliette.rousselet@etu.univ-orleans.fr>
2023-12-03 23:17:23 +0100

    jour 3 partie 1

--- jour3/code.py
+++ jour3/code.py
@@ -1,3 +1,67 @@
 def get_fichier(nom_fichier):
     with open(nom_fichier, 'r') as f:
         return f.read()
+
+def has_symbol_nearby(fichier, num_ligne, num_carac):
+    if num_ligne != 0:
+        if num_carac != 0 and fichier[num_ligne-1][num_carac-1] in symbols:
+            return True
+        if fichier[num_ligne-1][num_carac] in symbols:
+            return True
+        if num_carac != len(fichier[num_ligne])-1 and fichier[num_ligne-1][num_carac+1] in symbols:
+            return True
+    if num_carac != 0 and fichier[num_ligne][num_carac-1] in symbols:
+        return True
+    if num_carac != len(fichier[num_ligne])-1 and fichier[num_ligne][num_carac+1] in symbols:
+        return True
+    if num_ligne != len(fichier)-1:
+        if num_carac != 0 and fichier[num_ligne+1][num_carac-1] in symbols:
+            return True
+        if fichier[num_ligne+1][num_carac] in symbols:
+            return True
+        if num_carac != len(fichier[num_ligne])-1 and fichier[num_ligne+1][num_carac+1] in symbols:
+            return True
+    return False
+
+def somme_des_parties(nom_fichier):
+    fichier = get_fichier(nom_fichier).split("\n")
+    fichier.pop()
+    """fichier = ["467..114..",
+               "...*......",
+               "..35..633.",
+               "......#...",
+               "617*......",
+               ".....+.58.",
+               "..592.....",
+               "......755.",
+               "...$.*....",
+               ".664.598.."]"""
+    somme = 0
+    oks = []
+    current_number = ""
+    current_number_valid = False
+    for num_ligne in range(len(fichier)):
+        for num_carac in range(len(fichier[num_ligne])):
+            if fichier[num_ligne][num_carac].isdigit():
+                current_number += fichier[num_ligne][num_carac]
+                if not current_number_valid and has_symbol_nearby(fichier, num_ligne, num_carac):
+                    current_number_valid = True
+            else:
+                if current_number_valid:
+                    somme += int(current_number)
+                    oks.append(current_number)
+                current_number = ""
+                current_number_valid = False
+    return somme
+
+def all_caracs(nom_fichier):
+    fichier = get_fichier(nom_fichier)
+    caracs = set()
+    for ligne in fichier:
+        for carac in ligne:
+            caracs.add(carac)
+    return caracs
+
+symbols = ["@", "-", "*", "$", "/", "&", "+", "%", "=", "#"]
+print(somme_des_parties("jour3/input"))
+#517021
